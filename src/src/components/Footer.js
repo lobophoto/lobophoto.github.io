f9cc1705b025f71cf14dcbad0c99f6cef1059e0f
yangli@sensetime.com <yangli@sensetime.com>
2018-06-23 13:39:25 +0800

    add cover

--- src/src/components/Footer.js
+++ src/src/components/Footer.js
@@ -3,8 +3,14 @@ import React from 'react';
 class Footer extends React.Component{
   render(){
     return (
-      <div>
-      
+      <div style={{ bottom: 0, textAlign: 'left', color: 'white', width: '90%', margin: '20px auto', fontWeight: 'lighter' }}>
+        <div style={{ padding: '30px 0'}}>
+          <p style={{
+            fontSize: 12,
+            fontFamily: 'serif'
+          }}>联系方式</p>
+          <span style={{ fontSize: 12 }}>13212109806</span>
+        </div>
       </div>
     )
   }
--- src/src/routes/IndexPage.js
+++ src/src/routes/IndexPage.js
@@ -3,14 +3,61 @@ import { connect } from 'dva';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import styles from './IndexPage.less';
+import request from 'superagent';
+import _ from 'lodash';
 
-function IndexPage() {
-  return (
-    <div className={styles.normal}>
-      <Header />
-      <Footer />
-    </div>
-  );
+
+class IndexPage extends React.Component{
+  constructor(p){
+    super(p);
+    this.state = {
+      loading: false,
+      folders: {}
+    }
+  }
+
+  componentDidMount(){
+    request('./config.json').then(d => d.text).then(JSON.parse).then(obj => {
+      this.setState({
+        folders: obj.folders
+      });
+    });
+  }
+
+  render(){
+    const { folders } = this.state;
+    return (
+      <div className={styles.normal}>
+        <Header />
+        <div style={{ overflow: 'hidden'}}>
+          {
+            Object.keys(folders).map(key => {
+              return (
+                <div 
+                  className={ "col-md-6 col-lg-4 col-sm-12 col-xs-12 "  + styles.container}
+                  style={{ float: 'left', padding: 0, height: 250 }}
+                  key={key}
+                >
+                  <div className={styles.mask}>
+                    <a className={styles.button}> VIEW {key} </a>
+                  </div>                
+                  <div style={{
+                    height: '100%',
+                    width: '100%',
+                    background: `url(${_.get(folders[key], '[0]')})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat'
+                  }}/>
+                </div>
+              )
+            })
+          }
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 IndexPage.propTypes = {
